4230dd35ec72944cc31281ae942264159ba50237
nguyenthikimchi04 <kimchii28042003@gmail.com>
2024-01-04 11:39:08 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,41 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Splash from "./src/Screens/Splash";
+import Login from "./src/Screens/Login";
+import Signup from "./src/Screens/Signup";
+import Home from "./src/Screens/Home";
+import Details from "./src/Screens/Details";
+import Cart from "./src/Screens/Cart";
+import { Provider } from "react-redux";
+import { Store } from "./Redux/Store";
+import Orderplaced from "./src/Screens/Orderplaced";
+
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <Provider store={Store}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Slapsh"
+          screeeOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="Slapsh" component={Splash} options={{ headerShown: false }} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Signup" component={Signup} options={{ headerShown: false }} />
+          <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+          <Stack.Screen name="Details" component={Details} options={{ headerShown: false }} />
+          <Stack.Screen name="Cart" component={Cart} options={{ headerShown: false }} />
+          <Stack.Screen name="Orderplaced" component={Orderplaced} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
+};
+
+export default App;
---
+++ src/Utils/Date.js
@@ -0,0 +1,72 @@
+export const fruits = [
+  {
+    name: "Bánh Custian",
+    pieces: "7",
+    price: 4.939,
+    img: "https://images.foody.vn/res/g7/67000/s400x400/foody-harvest-baking-banh-mi-tuoi-ngon-414-637065026987741600.jpg",
+  },
+  {
+    name: "Bánh Tiramisu",
+    pieces: "7",
+    price: 4.999,
+    img: "https://daotaobeptruong.vn/wp-content/uploads/2020/11/banh-tiramisu.jpg",
+  },
+  {
+    name: "Bánh Kem",
+    pieces: "6",
+    price: 1.999,
+    img: "https://friendshipcakes.com/wp-content/uploads/2023/05/banh-tao-hinh-21.jpg",
+  },
+  
+  
+  {
+    name: "Bánh Matcha",
+    pieces: "5",
+    price: 4.999,
+    img: "https://friendshipcakes.com/wp-content/uploads/2023/05/banh-tao-hinh-21.jpg",
+  },
+];
+export const vegetables = [
+  {
+    name: "Bánh mì ",
+    pieces: "10",
+    price: 1.999,
+    img: "https://www.huongnghiepaau.com/wp-content/uploads/2022/10/cach-lam-banh-mi-ngot.jpg",
+  },
+  {
+    name: "Bánh su kem",
+    pieces: "4",
+    price: 4.939,
+    img: "https://www.lottemart.vn/media/catalog/product/cache/0x0/0/4/0400233890001.jpg.webp",
+  },
+  {
+    name: "Bánh kem dâu",
+    pieces: "7",
+    price: 4.999,
+    img: "https://www.lottemart.vn/media/catalog/product/cache/0x0/0/4/0400233890001.jpg.webp",
+  },
+  {
+    name: "Bánh sừng trâu",
+    pieces: "5",
+    price: 4.999,
+    img: "https://cdn.tgdd.vn/Files/2020/03/23/1243894/lam-banh-sung-trau-banh-croissant-trung-muoi-ng-760x367.jpg",
+  },
+];
+export const Dropdown = [
+  {
+   title:"Product-Details",
+    content:
+      "bánh được làm thủ công",
+  },
+  {
+    title:"Nutritions",
+    content:
+      "IPhone Pro Max",
+  },
+  {
+    title:"Reviews",
+    content:
+      "4-reviews iphone",
+  },
+  
+];
